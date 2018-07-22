e48a1ab84356ff22aa4394d94634fbbf229cd548
Steven Torrence <storrence88@gmail.com>
2018-07-21 23:20:53 -0500

    Updated server to respond with CSS

--- app.js
+++ app.js
@@ -9,8 +9,22 @@ let server = http.createServer((request, response) => {
       response.end();
     });
   }
+  else if (request.url === '/dojo.html') {
+    fs.readFile('dojo.html', 'utf8', (errors, contents) => {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(contents);
+      response.end();
+    });
+  } 
+  else if (request.url === '/styles.css') {
+    fs.readFile('styles.css', (errors, contents) => {
+      response.writeHead(200, {'Content-Type': 'text/css'});
+      response.write(contents);
+      response.end();
+    });
+  }
   else {
-    repsonse.writeHead(404);
+    response.writeHead(404);
     response.end('File not found!!!');
   }
 });
